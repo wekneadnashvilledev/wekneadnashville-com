77f7bb5f25dedccd74b1b2fce57b80cd1a9e84e8
Andrew Velez <andrewvelez79@gmail.com>
2012-12-16 20:03:03 -0500

    trying a different javascript solution

---
+++ wekneadnashvilleweb/scripts/sitemain.js
@@ -0,0 +1,11 @@
+﻿
+if (top.location != location) {
+    top.location.href = document.location.href;
+}
+
+try {
+    var currentState = {};
+    window.history.replaceState(currentState, "Title", "http://www.wekneadnashville.com" + document.location.pathname);
+} catch (error) {
+    alert(error.message);
+}
